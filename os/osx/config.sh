72de99b10471eb4a04c3a79ddb6a7e3306ef316e
Nick Reffitt <nickreffitt@gmail.com>
2015-12-13 11:25:33 +0000

    Config: Execute .zsh_profile when .zshrc runs

---
+++ os/osx/config.sh
@@ -0,0 +1,5 @@
+echo "source ~/Dropbox/.zsh_profile" >> ~/.zshrc
+
+echo "Setting up global git config"
+git config --global user.name "Nick Reffitt"
+git config --global user.email "nickreffitt@gmail.com"
