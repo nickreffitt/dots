e0ec5c825c7ed314d59169b5581c67e50e096406
Nick Reffitt <nickreffitt@gmail.com>
2015-12-20 17:01:30 +0000

    Set git to rebase by default when pulling, add "lola" alias

--- os/osx/config.sh
+++ os/osx/config.sh
@@ -6,3 +6,11 @@ echo ""
 echo "Setting up global git config"
 git config --global user.name "Nick Reffitt"
 git config --global user.email "nickreffitt@gmail.com"
+
+echo ""
+echo "Git: Rebase by default"
+git config --global pull.rebase true
+
+echo ""
+echo "Git: lola alias"
+git config --global alias.lola 'log --graph --decorate --pretty=oneline --abbrev-commit --all'"
